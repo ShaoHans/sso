06f437b035e80685fba731b35dce11385e3f9d88
邵汉中 <635260101@qq.com>
2018-06-01 17:35:46 +0800

    使用数据库配置IdentityServer

--- Ids4.Mvc.CenterWithAspNetIdentity/Startup.cs
+++ Ids4.Mvc.CenterWithAspNetIdentity/Startup.cs
@@ -11,6 +11,9 @@ using Microsoft.Extensions.DependencyInjection;
 using Ids4.Mvc.CenterWithAspNetIdentity.Data;
 using Ids4.Mvc.CenterWithAspNetIdentity.Models;
 using Ids4.Mvc.CenterWithAspNetIdentity.Services;
+using System.Reflection;
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
 
 namespace Ids4.Mvc.CenterWithAspNetIdentity
 {
@@ -38,18 +41,39 @@ namespace Ids4.Mvc.CenterWithAspNetIdentity
 
             services.AddMvc();
 
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
             services.AddIdentityServer()
                 .AddDeveloperSigningCredential()
                 .AddInMemoryPersistedGrants()
-                .AddInMemoryApiResources(Ids4Config.GetApiResources())
-                .AddInMemoryClients(Ids4Config.GetClients())
-                .AddInMemoryIdentityResources(Ids4Config.GetIdentityResources())
+                //.AddInMemoryApiResources(Ids4Config.GetApiResources())
+                //.AddInMemoryClients(Ids4Config.GetClients())
+                //.AddInMemoryIdentityResources(Ids4Config.GetIdentityResources())
+                .AddConfigurationStore(options =>
+                {
+                    // this adds the config data from DB (clients, resources)
+                    options.ConfigureDbContext = builder =>
+                        builder.UseSqlServer(Configuration.GetConnectionString("IdentityServerConfig"),
+                            sql => sql.MigrationsAssembly(migrationsAssembly));
+                })
+                .AddOperationalStore(options =>
+                {
+                    // this adds the operational data from DB (codes, tokens, consents)
+                    options.ConfigureDbContext = builder =>
+                        builder.UseSqlServer(Configuration.GetConnectionString("IdentityServerConfig"),
+                            sql => sql.MigrationsAssembly(migrationsAssembly));
+
+                    // this enables automatic token cleanup. this is optional.
+                    options.EnableTokenCleanup = true;
+                    options.TokenCleanupInterval = 30;
+                })
                 .AddAspNetIdentity<ApplicationUser>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            InitializeDatabase(app);
+
             if (env.IsDevelopment())
             {
                 app.UseBrowserLink();
@@ -72,6 +96,43 @@ namespace Ids4.Mvc.CenterWithAspNetIdentity
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+        }
+
+        private void InitializeDatabase(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+                var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+                context.Database.Migrate();
+                if (!context.Clients.Any())
+                {
+                    foreach (var client in Ids4Config.GetClients())
+                    {
+                        context.Clients.Add(client.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.IdentityResources.Any())
+                {
+                    foreach (var resource in Ids4Config.GetIdentityResources())
+                    {
+                        context.IdentityResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.ApiResources.Any())
+                {
+                    foreach (var resource in Ids4Config.GetApiResources())
+                    {
+                        context.ApiResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+            }
         }
     }
 }
